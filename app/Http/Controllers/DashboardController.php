d553010159e195fa7e7dbc6f31494abfd29bcad0
mdarifmia <arif@supremesupports.com.au>
2023-12-31 09:06:06 +0600

    create migration, create table, insert data to table, retrieved data from table, show retrieved data to blade view page, order data by created_at field

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view("dashboard");
+
+
+        $idea = new Idea([
+            "content"=>"This is a new idea."
+        ]);
+
+        $idea->save();
+
+        return view("dashboard",[
+            "ideas"=>Idea::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
